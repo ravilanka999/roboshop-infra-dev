f5bce1475f1facac83bf098842b0d9928843274f
ravilanka999 <ravi050892@gmail.com>
2025-07-17 11:17:15 +1000

    roboshop-infra-dev

---
+++ 30-vpn/variables.tf
@@ -0,0 +1,17 @@
+variable "project" {
+    default = "roboshop"
+  
+}
+
+variable "environment" {
+    default = "dev"
+}
+
+variable "zone_id" {
+    default = "Z01514503I0ST5YDDNHIX"
+}
+
+variable "zone_name" {
+    default = "sindhu.agency"
+}
+
