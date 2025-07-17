90fd23244f78c66768a1926b060fe7288082ec00
ravilanka999 <ravi050892@gmail.com>
2025-07-17 11:21:28 +1000

    roboshop-infra-dev

---
+++ 60-acm/variables.tf
@@ -0,0 +1,15 @@
+variable "project" {
+    default = "roboshop"
+}
+
+variable "environment" {
+    default = "dev"
+}
+
+variable "zone_id" {
+    default = "Z01514503I0ST5YDDNHIX"
+}
+
+variable "zone_name" {
+    default = "sindhu.agency"
+}
